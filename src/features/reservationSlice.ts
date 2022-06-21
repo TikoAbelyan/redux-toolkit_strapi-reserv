3d82e66f2d83a517a0d9883c23be386c378ddc04
tikoabelyan <tikoabelyan1995@gmail.com>
2022-06-22 01:28:08 +0400

    initial

---
+++ src/features/reservationSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface ReservationState {
+  value: string[];
+}
+
+const initialState: ReservationState = {
+  value: ["barev", "hajox"],
+};
+export const reservationSlice = createSlice({
+  name: "reservations",
+  initialState,
+  reducers: {
+    addReservation: (state, action: PayloadAction<string>) => {
+      state.value.push(action.payload);
+    },
+  },
+});
+
+export const { addReservation } = reservationSlice.actions;
+export default reservationSlice.reducer;
