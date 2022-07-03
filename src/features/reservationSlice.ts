8b81f81d03b01fab0c253e44750e8845c62db6e2
tikoabelyan <tikoabelyan1995@gmail.com>
2022-07-03 21:44:29 +0400

    docker host change

--- src/features/reservationSlice.ts
+++ src/features/reservationSlice.ts
@@ -7,7 +7,7 @@ interface ReservationState {
 
 const initialState: ReservationState = {
   value: [],
-  table: ["Table 1", "Table 2", "Table 3"],
+  table: ["Table 1", "Table 2", "Table 3", "Table 4", "Table 5"],
 };
 export const reservationSlice = createSlice({
   name: "reservations",
